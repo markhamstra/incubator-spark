41cd01ae5afca8d8fe56708b31d127ac5d5b7ab0
Mark Hamstra <markhamstra@gmail.com>
2013-11-19 10:16:48 -0800

    Actor instead of eventQueue for LocalJobCompleted

--- core/src/main/scala/org/apache/spark/scheduler/DAGScheduler.scala
+++ core/src/main/scala/org/apache/spark/scheduler/DAGScheduler.scala
@@ -635,7 +635,7 @@ class DAGScheduler(
       case e: Exception =>
         job.listener.jobFailed(e)
     } finally {
-      eventQueue.put(LocalJobCompleted(job.finalStage))
+      eventProcessActor ! LocalJobCompleted(job.finalStage)
     }
   }
 
